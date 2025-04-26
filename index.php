e6da07091a7de7bba14b1d4ac6b6862ecf2f482b
Jean <jean.dominguez.gonzalez21@gmail.com>
2025-04-26 10:29:46 -0500

    ya funciona mysql, validando postgre

--- index.php
+++ index.php
@@ -7,21 +7,21 @@ error_reporting(E_ALL);
 echo "<h2>Validando conexión a bases de datos...</h2>";
 
 // --- Primero PostgreSQL (puerto 5432 explícito) ---
-//$pg_conn = pg_connect(
-//    "host=10.161.2.4 " .
-//   "port=5432 " .
- //   "dbname=postgres " .
- //   "user=Tiancris " .
-//    "password=#Zarache060221 " .
-//    "sslmode=require"
-//);
+$pg_conn = pg_connect(
+   "host=10.167.2.4" .
+   "port=5432" .
+    "dbname=postgres" .
+    "user=rooot" .
+    "password=Rut12345" .
+    "sslmode=require"
+);
 
-//if ($pg_conn) {
- //   echo "<p style='color:green;'>✅ Conexión exitosa a PostgreSQL</p>";
- //   pg_close($pg_conn);
-//} else {
-//    echo "<p style='color:red;'>❌ Error PostgreSQL: " . pg_last_error() . "</p>";
-//}
+if ($pg_conn) {
+   echo "<p style='color:green;'>✅ Conexión exitosa a PostgreSQL</p>";
+   pg_close($pg_conn);
+} else {
+   echo "<p style='color:red;'>❌ Error PostgreSQL: " . pg_last_error() . "</p>";
+}
 
 // --- Luego MySQL (puerto 3306 explícito) ---
 $con = mysqli_init();

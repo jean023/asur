1ebea026d0fcf9f30467c55c5aa12f902d066213
Jean <jean.dominguez.gonzalez21@gmail.com>
2025-04-25 22:39:37 -0500

    c

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $con = mysqli_init();
 mysqli_ssl_set($con, NULL, NULL, NULL, NULL, NULL);
-mysqli_real_connect($con, "server1.mysql.database.azure.com", "rooot", "Rut12345", "main", 3306, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, "server1.mysql.database.azure.com", "rooot", "Rut12345", "main", 3306, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno()) {
     die("❌ Conexión fallida: " . mysqli_connect_error());

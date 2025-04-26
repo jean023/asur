9f6092e3ed808b6769a68740cb4cc80780d98193
Jean <jean.dominguez.gonzalez21@gmail.com>
2025-04-25 22:07:44 -0500

    a

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-$host = '10.161.2.4';       // IP privada o DNS interno del MySQL Flexible Server en PRD
+$host = 'serverl.mysql.database.azure.com';       // IP privada o DNS interno del MySQL Flexible Server en PRD
 $user = 'rooot';            // Tu usuario de MySQL
 $pass = 'Rut12345';         // Tu contraseña
 $db   = 'mysqlprod21';      // Tu base de datos en MySQL
 
 // Conexión
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db,3306,MYSQLI_CLIENT_SSL);
 
 // Validar conexión
 if ($conn->connect_error) {

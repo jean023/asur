27e59c84628fd5f521060f40c19c0e55ced4e717
Jean <jean.dominguez.gonzalez21@gmail.com>
2025-04-25 23:45:37 -0500

    f

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-$con = mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, NULL, NULL, NULL);
-mysqli_real_connect($con, "serverk.mysql.database.azure.com", "rooot", "Rut12345", "sys", 3306, MYSQLI_CLIENT_SSL);
-
-if (mysqli_connect_errno()) {
-    die("❌ Conexión fallida: " . mysqli_connect_error());
-} else {
-    echo "✅ Conectado correctamente a MySQL.";
-}
+echo("esto no funciona aun pero se ve muy bien ")
 ?>

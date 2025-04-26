8a616c54d4a3b4ab4af0e0a65b817ce7aa821d85
Jean <jean.dominguez.gonzalez21@gmail.com>
2025-04-25 22:01:00 -0500

    nvrmnd1

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "Hola desde Azure App Service en PHP";
+$host = '10.161.2.4';       // IP privada o DNS interno del MySQL Flexible Server en PRD
+$user = 'rooot';            // Tu usuario de MySQL
+$pass = 'Rut12345';         // Tu contraseña
+$db   = 'mysqlprod21';      // Tu base de datos en MySQL
+
+// Conexión
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Validar conexión
+if ($conn->connect_error) {
+    die("❌ Error de conexión: " . $conn->connect_error);
+} else {
+    echo "✅ Conexión exitosa a MySQL desde App Service.<br>";
+}
+
+$conn->close();
 ?>
